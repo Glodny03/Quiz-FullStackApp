6ef42e2c7af6355317d6f45bfef957ec7153610e
Jakub Mrzyglod <jakubmrzyglod03@gmail.com>
2021-07-07 08:49:29 +0200

    Adding item elements

--- app.js
+++ app.js
@@ -17,9 +17,19 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//POBIERANIE AKTURALNEGO ADRESU STRONY
+app.use(function (req, res, next) {
+  res.locals.path = req.path;
+  // console.log(`Aktualna sciezka to: ${req.path}`)
+  //BEZ WYWOŁANIA FUNKCJI NEXT SERWER SIĘ ZAWIESZA BO NIE PRZECHODZI DALEJ!
+  next();
+})
 
 app.use('/', indexRouter);
 app.use('/news', newsRouter);
@@ -27,12 +37,12 @@ app.use('/quiz', quizRouter);
 app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
