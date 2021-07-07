0905ec5758fa21b19a4bc1b4621a7e2c0c6d63e7
Jakub Mrzyglod <jakubmrzyglod03@gmail.com>
2021-07-07 10:12:59 +0200

    Refactoring the administrator login panel

--- routes/admin.js
+++ routes/admin.js
@@ -1,24 +1,17 @@
-const express = require('express');
-const { NotExtended } = require('http-errors');
-const router = express.Router();
+const express = require("express"),
+  {
+    NotExtended: NotExtended
+  } = require("http-errors"),
+  router = express.Router();
 
-router.all('*', (req, res, next) => {
-  if(!req.session.admin) {
-    res.redirect('login');
+router.all("*", (req, res, next) => {
+    req.session.admin ? next() : res.redirect("login")
+  }),
 
-    return;
-  }
+  router.get("/", (req, res) => {
+    console.log(req.session.admin), res.render("admin", {
+      title: "Admin"
+    })
+  }),
 
-  next();
-});
-
-/* GET home  page. */
-router.get('/', (req, res) => {
-  console.log(req.session.admin);
-
-  res.render('admin', {
-    title: 'Admin'
-  });
-});
-
-module.exports = router;
+  module.exports = router;
